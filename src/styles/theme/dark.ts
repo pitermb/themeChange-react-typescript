a0ef0d6216882596e9828c20fec47ab2c8552c15
Piter Merlo Bruner <68957875+pitermb@users.noreply.github.com>
2022-06-14 09:39:44 -0300

    Add files via upload

---
+++ src/styles/theme/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primay: '#333',
+        secundary: '#D63AF9',
+
+        background: '#222',
+        text: '#fff',
+    },
+};
---
+++ src/styles/theme/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+        primay: '#C61E65',
+        secundary: '#D63AF9',
+
+        background: '#f5f5f5',
+        text: '#333',
+    },
+};
